3efc6ffb2c3bd096fb883684e7cf6882c6ecd705
juliely8619 <july8619@yahoo.com>
2019-03-08 11:02:39 -0800

    made simple hello world app

--- PiPA/Startup.cs
+++ PiPA/Startup.cs
@@ -15,11 +15,21 @@ namespace PiPA
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                name: "default",
+                template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.UseStaticFiles();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
