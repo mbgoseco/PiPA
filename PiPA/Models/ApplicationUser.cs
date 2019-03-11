ca2742f362a75d4b22a25990d79a43bb95c9bfc2
juliely8619 <july8619@yahoo.com>
2019-03-11 10:21:13 -0700

    added application db

--- PiPA/Models/ApplicationUser.cs
+++ PiPA/Models/ApplicationUser.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace PiPA.Models
 {
-    public class ApplicationUser
+    public class ApplicationUser : IdentityUser
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
