7cddd54806fdf3cb33e0e7c2ac4ef19a0c8c5391
Andrew Hinojosa <drewsview34@gmail.com>
2019-03-11 15:38:58 -0700

    Created Account Controller. Pair Programming with July to refactor.

--- PiPA/Models/ApplicationUser.cs
+++ PiPA/Models/ApplicationUser.cs
@@ -11,5 +11,6 @@ namespace PiPA.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
+        public DateTime Birthday { get; set; }
     }
 }
