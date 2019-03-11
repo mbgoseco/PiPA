342f2415c5f2d96b3e6ba27ecd14caa5d551b62d
Andrew Hinojosa <drewsview34@gmail.com>
2019-03-11 13:51:30 -0700

    Created LoginView Models, Register, and AccountController

---
+++ PiPA/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PiPA.Models.ViewModels
+{
+    /// <summary>
+    /// Defines the <see cref="LoginViewModel" />
+    /// </summary>
+    public class LoginViewModel
+    {
+        /// <summary>
+        /// Gets or sets the Email
+        /// </summary>
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Password
+        /// </summary>
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
