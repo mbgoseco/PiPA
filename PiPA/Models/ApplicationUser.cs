08a76594f74323c9d468e268394e21dc4e45a809
juliely8619 <july8619@yahoo.com>
2019-03-11 09:46:36 -0700

    added some properties for user (for in addition to identity)

--- PiPA/Models/ApplicationUser.cs
+++ PiPA/Models/ApplicationUser.cs
@@ -7,5 +7,8 @@ namespace PiPA.Models
 {
     public class ApplicationUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
     }
 }
