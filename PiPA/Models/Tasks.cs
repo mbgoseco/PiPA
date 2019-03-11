3016ce7193554e2aa784e0e1f2f7be562872f3fa
juliely8619 <july8619@yahoo.com>
2019-03-11 09:41:38 -0700

    populated task class

--- PiPA/Models/Tasks.cs
+++ PiPA/Models/Tasks.cs
@@ -7,5 +7,13 @@ namespace PiPA.Models
 {
     public class Tasks
     {
+        public int ID { get; set; }
+        public int ListID { get; set; }
+        public string TaskName { get; set; }
+        public string Description { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime PlannedDateComplete { get; set; }
+        public DateTime CompletedDate { get; set; }
+        public bool IsComplete { get; set; }
     }
 }
