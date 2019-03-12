737a0d98855a0cb84f5b1c16e8b5ec7ff74c8f92
Michael Filicetti <filicetti.michael@gmail.com>
2019-03-11 17:40:57 -0700

    Added production connection strings and fixed wwwroot directory

--- PiPA/Startup.cs
+++ PiPA/Startup.cs
@@ -28,8 +28,7 @@ namespace PiPA
             builder.AddUserSecrets<Startup>();
             Configuration = builder.Build();
         }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
